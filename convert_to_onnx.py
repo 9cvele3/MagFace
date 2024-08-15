a3170c36a694b12d311a1bab29ad4433954e69d7
Dusan Cvetkovic <9cvele3@gmail.com>
2024-08-15 13:24:58 +0200

    Fixes conversion for iresnet100

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -8,12 +8,12 @@ import onnx
 from inference import network_inf
 
 parser = argparse.ArgumentParser(description='Test')
-parser.add_argument('-m', '--trained_model', default='magface_iresnet18_casia_dp.pth',
+parser.add_argument('-m', '--trained_model', default='magface_epoch_00025.pth',
                     type=str, help='Trained state_dict file path to open')
-parser.add_argument('--arch', default='iresnet18', help='Backbone network mobile0.25 or resnet50')
+parser.add_argument('--arch', default='iresnet100', help='Backbone network mobile0.25 or resnet50')
 parser.add_argument('--embedding_size', default=512, type=int,
                     help='The embedding feature size')
-parser.add_argument('--cpu', action="store_true", default=False, help='Use cpu inference')
+parser.add_argument('--cpu', action="store_true", default=True, help='Use cpu inference')
 
 args = parser.parse_args()
 
@@ -21,20 +21,24 @@ args = parser.parse_args()
 def check_keys(model, pretrained_state_dict):
     ckpt_keys = set(pretrained_state_dict.keys())
     model_keys = set(model.state_dict().keys())
+    print(f"model_keys: { list(model_keys)[0:10] }")
+    print(f"ckpt_keys: { list(ckpt_keys)[0:10] }")
     used_pretrained_keys = model_keys & ckpt_keys
     unused_pretrained_keys = ckpt_keys - model_keys
     missing_keys = model_keys - ckpt_keys
+    print('Model keys:{}'.format(len(model_keys)))
+    print('ckpt keys:{}'.format(len(ckpt_keys)))
     print('Missing keys:{}'.format(len(missing_keys)))
     print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
     print('Used keys:{}'.format(len(used_pretrained_keys)))
     assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
-    return True
+    return len(used_pretrained_keys) > 0
 
 
 def remove_prefix(state_dict, prefix):
     ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
     print('remove prefix \'{}\''.format(prefix))
-    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    f = lambda x: x.replace(prefix, "", 1) if x.startswith(prefix) or x.startswith("features." + prefix) else x
     return {f(key): value for key, value in state_dict.items()}
 
 
@@ -45,11 +49,23 @@ def load_model(model, pretrained_path, load_to_cpu):
     else:
         device = torch.cuda.current_device()
         pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+
+    print(pretrained_dict.keys())
+    #if "state_dict" in pretrained_dict.keys():
+    #    print(list(pretrained_dict['state_dict'].keys())[0:10])
+    #    pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'features.')
+    #else:
+    #    pretrained_dict = remove_prefix(pretrained_dict, 'features.')
+
     if "state_dict" in pretrained_dict.keys():
+        print(list(pretrained_dict['state_dict'].keys())[0:10])
         pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
     else:
         pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+
+    print(list(pretrained_dict.keys())[0:10])
     check_keys(model, pretrained_dict)
+
     # size mismatch for fc.weight: copying a param with shape torch.Size([512, 10572]) from checkpoint,
     # the shape in current model is torch.Size([512, 25088]).
     model.load_state_dict(pretrained_dict, strict=False)
